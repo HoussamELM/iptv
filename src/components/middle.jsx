73c85e30a5f42336dfb5d5e71576a958b5a65c31
houssamelm <houssamelmasoudi@gmail.com>
2022-10-20 22:55:47 +0100

    different mobile picture

--- src/components/middle.jsx
+++ src/components/middle.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import tv from '../pictures/iptv.png';
-import tv2 from '../pictures/iptv.png';
+import tv2 from '../pictures/iptvmb.png';
 import logo2 from '../pictures/TFAREJ-IPTV.png';
 import checkmark from '../pictures/check-mark.png';
 
