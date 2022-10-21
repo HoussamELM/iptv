bbeefa87b772d150c1718250cb20218350509ca8
houssamelm <houssamelmasoudi@gmail.com>
2022-10-21 18:53:29 +0100

    quick path fix

--- src/components/middle.jsx
+++ src/components/middle.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import tv from '../pictures/iptv.png';
-import tv2 from '../pictures/iptvmb2.png'; 
+import tv2 from '../pictures/iptvmb2.png';
 import arrowhead from "../pictures/arrowhead.png"
 import logo2 from '../pictures/TFAREJ-IPTV.png';
 import checkmark from '../pictures/check-mark.png';
