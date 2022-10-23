1af7667f7740cee5d6070883f05a184e57766e2e
houssamelm <houssamelmasoudi@gmail.com>
2022-10-23 22:45:46 +0100

    clean plan cards

--- src/components/plans.jsx
+++ src/components/plans.jsx
@@ -3,9 +3,70 @@ import React from 'react'
 const Plans = () => {
   return (
 <div className='plans' id="plans">
-  <div></div>
-  <div></div>
-  <div></div>
+  <div className="card">
+  <div>
+  <h5>MENSUEL</h5>
+    <p>abbonement (payé en avance)</p>
+    <h1>150 DH <span>/mois</span></h1>
+    <hr/>
+    <ul>
+      <li>20000 chaine TV</li>
+      <li>50000 VOD(Netflix...)</li>
+      <li>1 utilisateur</li>
+    </ul>
+    <div className='choose'>
+    <a target="_blank" href="https://wa.me/+212619834123?text=Bonjour! je veux commander un plan d'un seul mois">choisir</a>
+    </div>
+  </div>
+  </div>
+  <div className="card">
+  <div>
+  <h5>3 Mois</h5>
+    <p>abbonement (payé en avance)</p>
+    <h1>100 DH <span>/mois</span></h1>
+    <hr/>
+    <ul>
+      <li>20000 chaine TV</li>
+      <li>50000 VOD(Netflix...)</li>
+      <li>1 utilisateur</li>
+    </ul>
+    <div className='choose'>
+    <a target="_blank" href="https://wa.me/+212619834123?text=Bonjour! je veux commander un plan de 3 mois">choisir</a>
+    </div>
+  </div>
+  </div>
+  <div className="card">
+  <div>
+  <h5>6 Mois</h5>
+    <p>abbonement (payé en avance)</p>
+    <h1>75 DH <span>/mois</span></h1>
+    <hr/>
+    <ul>
+      <li>20000 chaine TV</li>
+      <li>50000 VOD(Netflix...)</li>
+      <li>1 utilisateur</li>
+    </ul>
+    <div className='choose'>
+    <a target="_blank" href="https://wa.me/+212619834123?text=Bonjour! je veux commander un plan de 6 mois">choisir</a>
+    </div>
+  </div>
+  </div>
+  <div className="card">
+  <div>
+  <h5>12 Mois</h5>
+    <p>abbonement (payé en avance)</p>
+    <h1>50 DH <span>/mois</span></h1>
+    <hr/>
+    <ul>
+      <li>20000 chaine TV</li>
+      <li>50000 VOD(Netflix...)</li>
+      <li>1 utilisateur</li>
+    </ul>
+    <div className='choose'>
+    <a target="_blank" href="https://wa.me/+212619834123?text=Bonjour! je veux commander un plan de 12 mois">choisir</a>
+    </div>
+  </div>
+  </div>
 </div>
   )
 }
