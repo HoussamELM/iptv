84dbf1a8e55c6a88b7341d807799818f19ea52de
houssamelm <houssamelmasoudi@gmail.com>
2022-10-24 06:42:28 +0100

    sass and compressed images

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./components/middle"
 import Navbar from './components/navbar';
 import Essaie from "./components/essaie";
-import './App.css'
+import './App.sass'
 
 
 function App() {
